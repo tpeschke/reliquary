ee9372aa212ef4cf209ae5c05fa065154f7441fe
Trent <mr.peschke@gmail.com>
2022-12-06 17:26:56 -0700

    set up chance table logic

--- server/reliquary.js
+++ server/reliquary.js
@@ -9,11 +9,13 @@ const app = new express()
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/api/getChanceTables', uniqueItemCtrl.getChanceTables)
 app.get('/api/getUniquieItem', uniqueItemCtrl.getRandomUniqueItem)
 
 massive(connection).then(dbI => {
     app.set('db', dbI)
     app.listen(3434, _ => {
+        uniqueItemCtrl.setUpTables()
         console.log(`One Thousand Good Deeds Cannot Blot Out the Stain of One Sin 3434`)
     })
 })
--- server/uniqueItemController.js
+++ server/uniqueItemController.js
@@ -2,10 +2,33 @@ const tables = require('./tables')
 
 let chanceTables = {}
 
+function getRandomIndex(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getRandomNumber(max) {
+    return Math.floor(Math.random() * (max - 1) + 1);
+}
+
 module.exports = {
-    getRandomUniqueItem: (req, res) => {
-        let table = tables.start[0].entry
-        res.send(tables[table][0])
+    setUpTables: () => {
+        for (const table in tables ) {
+            chanceTables[table] = []
+            tables[table].forEach(entry => {
+                for(let i = 0; i < entry.weight; i++) {
+                    chanceTables[table].push(entry.entry)
+                }
+            })
+        }
+        console.log('Chance Tables Set Up')
     },
+    getRandomUniqueItem: (req, res) => {
+        let table = chanceTables.start[getRandomIndex(chanceTables.start.length)]
+        let item = chanceTables[table][getRandomIndex(chanceTables[table].length)]
+        res.send(item)
+    },
+    getChanceTables: (req, res) => {
+        res.send(chanceTables)
+    }
 }
 
