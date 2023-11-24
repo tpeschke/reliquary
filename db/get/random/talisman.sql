b2664be866ef9cbf62ce8032e693af410061427d
Trent <mr.peschke@gmail.com>
2023-11-24 12:47:29 -0700

    bug fixing

--- db/get/random/talisman.sql
+++ db/get/random/talisman.sql
@@ -1,2 +1,3 @@
 select *, concat('When you fail a ', concat(r.skill, ' Check, you may gain +3 but have this talisman break & still gain a Complication, even if you succeed. You can only apply 1 talisman / Check.')) as explanation from rtalisman r
 order by random()
+limit $1
